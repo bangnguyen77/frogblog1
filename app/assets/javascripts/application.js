b69d6d053a394a8ff5f815a844b98e989eedccaa
Bang Nguyen <bangnv@gmail.com>
2016-09-10 18:17:19 -0700

    add some plugin for the text editor

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,5 +18,9 @@
 
 document.addEventListener("turbolinks:load", function() {
   tinymce.remove();
-  tinymce.init({ selector:'textarea' });
+  tinymce.init({
+    selector:'textarea#post_body',
+    plugins: "codesample image",
+    toolbar: "undo redo | styleselect | bold italic | codesample image"
+  });
 })
