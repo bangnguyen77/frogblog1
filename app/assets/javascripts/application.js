4c57da83ea63665797eadfbd49f58c5f6fef4eda
Bang Nguyen <bangnv@gmail.com>
2016-09-10 18:30:11 -0700

    add some more plugins

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,7 +20,7 @@ document.addEventListener("turbolinks:load", function() {
   tinymce.remove();
   tinymce.init({
     selector:'textarea#post_body',
-    plugins: "codesample image",
-    toolbar: "undo redo | styleselect | bold italic | codesample image"
+    plugins: "codesample image media link code",
+    toolbar: "undo redo | styleselect | bold italic link | codesample image media | code"
   });
 })
